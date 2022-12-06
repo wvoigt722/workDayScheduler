d6340331f22cc69a689555b82daaf287e90392e8
unknown <wvoigt722@gmail.com>
2022-12-06 15:11:49 -0800

    added divs in HTML for time slots and changed time clock in JS

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// in the html
+
+ // TODO: Add code to display the current date in the header of the page.
+
+ var timeDisplayEl = document.querySelector('#currentDay');
+
+ function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.textContent = rightNow;
+}
+
+setInterval(displayTime, 1000);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -19,10 +31,6 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-
-var today = dayjs();
-document.querySelector('#currentDay').textContent =(today.format('MMM D, YYYY'));
 
 
 });
